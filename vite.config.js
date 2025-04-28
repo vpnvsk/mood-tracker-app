b8e71d82e98288a31d27d4d491dcd851575cbca2
vpnvsk <vlad.pnevsk@gmail.com>
2025-04-28 05:18:13 +0200

    fix: fixed deps

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-  },
+  base: "/mood-tracker-app"
 })
