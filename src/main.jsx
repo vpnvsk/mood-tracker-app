28bc6c6cd6b08d7d47a7b24a9da5292036d505c6
vpnvsk <vlad.pnevsk@gmail.com>
2025-04-28 05:26:21 +0200

    fix: fixed deps

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Use HashRouter instead of BrowserRouter
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
